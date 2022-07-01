ef9355d1996abf872721595db71949d49a9f08da
Hassel1999 <hasselaguilera@gmail.com>
2022-06-30 19:41:29 -0600

    Pantalla principal, formularios y clase persona

--- prestacionesEmpleados/Form1.cs
+++ prestacionesEmpleados/Form1.cs
@@ -19,9 +19,11 @@ namespace prestacionesEmpleados
 
         private void btnSalir_Click(object sender, EventArgs e)
         {
+            //confirmacion si eso deseea hacer una limpieza
             DialogResult=(MessageBox.Show("Desea limpiar los campos?"," ",MessageBoxButtons.YesNo,MessageBoxIcon.Question));
             if (DialogResult == DialogResult.Yes)
             {
+                //limpieza de los textbox
                 foreach (Control ctrl in this.Controls)
                 {
                     if (ctrl is TextBox)
@@ -35,6 +37,7 @@ namespace prestacionesEmpleados
 
         private void txtCodigo_KeyPress(object sender, KeyPressEventArgs e)
         {
+            //validacion del textbox codigo (solo acepta numero y borrar)
             if (!(char.IsDigit(e.KeyChar)) && (e.KeyChar != (char)Keys.Back))
             {
                 MessageBox.Show("Solo puede ingresar numeros", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
@@ -44,6 +47,7 @@ namespace prestacionesEmpleados
 
         private void txtNombre_KeyPress(object sender, KeyPressEventArgs e)
         {
+            //validacion de textbox nombre (solo acepta letras, espacio y borrar)
             if (!(char.IsLetter(e.KeyChar)) && (e.KeyChar != (char)Keys.Back) && (e.KeyChar != (char)Keys.Space))
             {
                 MessageBox.Show("Solo puede ingresar letras", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
@@ -53,6 +57,7 @@ namespace prestacionesEmpleados
 
         private void txtApellido_KeyPress(object sender, KeyPressEventArgs e)
         {
+            //validacion de textbox apellido (solo acepta letras, espacio y borrar)
             if (!(char.IsLetter(e.KeyChar)) && (e.KeyChar != (char)Keys.Back) && (e.KeyChar != (char)Keys.Space))
             {
                 MessageBox.Show("Solo puede ingresar letras", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
@@ -62,28 +67,34 @@ namespace prestacionesEmpleados
 
         private void cmbSeleccion_SelectedIndexChanged(object sender, EventArgs e)
         {
+            //usamos try para detectar alguna exepcion
             try
             {
+                //instancia de los formularios
                 frmAdmin admin = new frmAdmin();
                 frmDocente docente = new frmDocente();
 
+                //variable para tomar el valor de la seleccion del combobox
                 int indice;
 
+                //igualacion de la variable con el valor que se ha seleccionado
                 indice=cmbSeleccion.SelectedIndex;
 
-                if(indice != -1)
+                //Si hay algo seleccionado se ejecutara esta parte del codigo
+                if(indice != -1)//si es diferente de -1 hay algo seleccionado
                 {
                     switch (indice)
                     {
-                        case 0:
-                            admin.ShowDialog();
+                        case 0://en caso de que sea el primero la variable indice debe tomar el valor de 0
+                            admin.ShowDialog();//Y se mostrara el formilario de administrador
                             break;
-                        case 1:
-                            docente.ShowDialog();
+                        case 1://en caso de que sea el primero la variable indice debe tomar el valor de 1
+                            docente.ShowDialog();//Y se mostrara el formulario de docente
                             break;
                     }
                 }
             }
+            //en caso de que se encuentre una excepcion se mostrara este mensaje
             catch
             {
                 MessageBox.Show("Por favor, No deje campos vacios", "Advertencia",MessageBoxButtons.OK,MessageBoxIcon.Warning);
--- prestacionesEmpleados/clsPersona.cs
+++ prestacionesEmpleados/clsPersona.cs
@@ -8,10 +8,12 @@ namespace prestacionesEmpleados
 {
     public class clsPersona
     {
+        //atributos
         private static double codigo;
         private static double nombre;
         private static double apellido;
 
+        //propiedades accesora
         public static double Codigo { get => codigo; set => codigo = value; }
         public static double Nombre { get => nombre; set => nombre = value; }
         public static double Apellido { get => apellido; set => apellido = value; }
